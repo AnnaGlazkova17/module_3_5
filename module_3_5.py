af55dcc2b0559c1f016d0b30f6ca760b25e102d9
AnnaGlazkova17 <glazkova1706@gmail.com>
2024-10-09 00:29:14 +0300

    исправлен, если последняя цифра 0

--- module_3_5.py
+++ module_3_5.py
@@ -2,8 +2,10 @@ def get_multiplied_digits(number):
     number = int(number) # преобразуем в число, автоматически уберутся нули, если они были в начале числа
     str_number = str(number) # преобразуем переменную в сторку, чтобы вытащить первую цифру, которая точно не ноль
     first = int(str_number[0]) # присваиваем переменной первую цифру
-    if len(str_number) == 1: # условие для выхода из рекурсии, если длина стороки равно 1 символу
+    if len(str_number) == 1 and first != 0: # условие для выхода из рекурсии, если длина стороки равно 1 символу
         return first # возвращаем только последнюю цифру
+    elif len(str_number) == 1 and first == 0: # если последня цифра ноль, то возвращаем 1
+        return 1
     return first * get_multiplied_digits(int(str_number[1:])) # рекурсия по условия задачи
 
 result = get_multiplied_digits(40203)
